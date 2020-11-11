38e53d75718785bb54285d6873f1c3a9762f50e0
peter_alfie <alfie1104@gmail.com>
2020-11-11 12:46:48 +0900

    1.0 Apollo Server Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
+//main.ts로 import되는 유일한 모듈
+//따라서 graphQL모듈도 AppModule에 추가해야함
+//GraphQLMoudle.forRoot() : root module을 설정하는 것
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [GraphQLModule.forRoot()],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//Application을 시작시키는 부분
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
